d3a0e923968df6c0045beed559dadd7709d9cea2
Diya Barahate <66784537+Diya0812@users.noreply.github.com>
2026-05-01 18:51:17 +0530

    Add files via upload
    this file include details of company

---
+++ AboutUs.jsx
@@ -0,0 +1,18 @@
+
+   
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>About Paradise Nursery</h2>
+      <p>
+        Paradise Nursery offers a wide variety of indoor and outdoor plants
+        to enhance your living space. We focus on quality and sustainability.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
+
